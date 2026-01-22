3a8104c777304f0e773c45afec3e3091d5bffcd4
Mulualem <mulualemdarimo76@gmail.com>
2026-01-22 21:21:36 +0100

    updates

--- notes/TokenShop.sol
+++ notes/TokenShop.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+pragma solidity ^0.8.20;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {
+    SafeERC20
+} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TokenShop {
     using SafeERC20 for IERC20;
