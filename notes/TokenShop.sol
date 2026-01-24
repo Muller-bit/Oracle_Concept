00b687eb1c37c8ec89c41f9ad602f9bca31488f5
Mulualem <mulualemdarimo76@gmail.com>
2026-01-24 23:35:26 +0100

    update

--- notes/TokenShop.sol
+++ notes/TokenShop.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: MIT
+/* // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+ pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {
@@ -60,3 +60,4 @@ contract TokenShop {
         emit OwnerWithdrawn(balance);
     }
 }
+*/
